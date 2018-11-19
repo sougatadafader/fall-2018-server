3d0c4b74a0edb68f71fb809b0e63dec96c7425ba
sougatadafader <sougatadafader@gmail.com>
2018-11-18 23:48:28 -0500

    changes

--- src/main/java/com/example/whiteboardfall2018serverjava/models/ParagraphWidget.java
+++ src/main/java/com/example/whiteboardfall2018serverjava/models/ParagraphWidget.java
@@ -6,6 +6,14 @@ import javax.persistence.Entity;
 public class ParagraphWidget extends Widget{
 	private String text;
 	
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
 	public void update(ParagraphWidget paragraphWidget) {
 		this.title = paragraphWidget.title != null ? paragraphWidget.title : this.title;
 		this.text = paragraphWidget.text != null ? paragraphWidget.text : this.text;
